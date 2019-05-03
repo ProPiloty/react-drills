d7a17888ada5d6dd6a4c56d3f91815a3d83ce17b
Tyler Ellingford <tyler@Rickys-MacBook-Pro.local>
2019-05-03 08:57:22 -0600

    completed app-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      list: ['Whisky', 'Lima', 'Hotel', 'Five'],
+      filterString: '',
+    }
+  }
+
+  handleChange(filterText){
+    this.setState({ filterString: filterText });
+  }
+
   render() {
+
+    let listToDisplay = this.state.list
+      .filter( (item, i) => {
+        return item.includes(this.state.filterString);
+      } )
+      .map( (item, i) => {
+        return <h2 key={i}>{item}</h2>
+      } );
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type='text' onChange={ e => this.handleChange(e.target.value)}/>
+        {listToDisplay}
       </div>
     );
   }
